a1c4e7502e98e90b3176dd70bc0a69ac86caaff1
Anand <asb761@nyu.edu>
2018-04-25 01:31:29 -0400

    Shiny App

---
+++ dataviz_app/server.R
@@ -0,0 +1,133 @@
+library(shiny)
+library(leaflet)
+library(sp)
+
+shinyServer(function(input, output, session) {
+  
+  output$nymap <- renderLeaflet({
+    map <- leaflet() %>%
+      addTiles("http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png") %>%
+      setView(lng = -74.0156491, lat = 40.7022541, zoom = 10)
+    map
+  })
+  
+  data_replace_mean<- function(data){
+    for(i in 1:ncol(data)){
+      if((class(data[,i]) == "numeric") | (class(data[,i])=="integer")){
+        data[is.na(data[,i]), i] <- mean(data[,i], na.rm = TRUE)
+      }
+    }
+    
+    for(i in 1:nrow(data)){
+      if((data[i,"calculated_host_listings_count"] > 1)  &  (data[i,"availability_365"]/365 < 0.3) & 
+         (data[i,"review_scores_rating"]>50)){
+            data[i,"is_commercial"] = "Commercial Listing"  
+      }
+      else{
+            data[i,"is_commercial"] = "Household Listing"
+      }
+    }
+    
+    return(data)
+  }
+  
+  file_change <- reactive({
+    file_to_read <- paste("../data/",as.character(input$month),"_",as.character(input$year),"_listings.csv",sep="")
+    col_keep <- c("character","NULL","NULL","NULL","NULL","character","NULL","NULL","NULL","NULL","NULL","NULL",
+                  "NULL","NULL","NULL","NULL","character","NULL","character","NULL","NULL","NULL","NULL","NULL",
+                  "NULL","NULL","NULL","NULL","NULL","NULL","NULL","NULL","NULL","NULL","NULL","NULL",
+                  "character","character","character","character","NULL","NULL","character","NULL","NULL",
+                  "numeric","numeric","NULL","character","character","NULL","numeric","numeric","NULL",
+                  "NULL","NULL","NULL","character","NULL","NULL","NULL","NULL","NULL","NULL","NULL","NULL",
+                  "NULL","NULL","numeric","numeric","numeric","numeric","NULL","NULL","NULL","NULL",
+                  "numeric","numeric","numeric","numeric","numeric","numeric","numeric","NULL","NULL",
+                  "NULL","NULL","NULL","NULL","NULL","numeric","NULL")
+    df <- read.csv(file_to_read,colClasses = col_keep)
+    df <- data_replace_mean(df)
+
+    df$avg_review_value <- ((df$review_scores_accuracy + df$review_scores_cleanliness + 
+                               df$review_scores_checkin + df$review_scores_communication + 
+                               df$review_scores_location + df$review_scores_value)/6)
+    
+    df$avg_review_value[is.na(df$avg_review_value) ]<- 0
+    
+    
+    df$avg_review_value = df$avg_review_value*10
+    df$price <- gsub("[$|,]", "", df$price)
+    df$price <- as.numeric(df$price)
+    return(df)
+  })
+  
+  observeInputs <- observeEvent(input$submit,{
+    df = file_change()
+    #print(input$price)
+    
+    df_subset = subset(df,(df$price > input$price) & (df$review_scores_rating > input$quality) & 
+                         (df$bedrooms > input$bedrooms) & (df$bathrooms > input$bathrooms) )
+    map <- leafletProxy("nymap", session) %>%
+      clearShapes() %>%
+      clearControls() %>%
+      addCircles(lng = df_subset$longitude,lat = df_subset$latitude)
+  })
+  
+  observeEvent(input$submit2,{
+    df = file_change()
+    
+    if(input$in_checkboxgroup=="CL"){
+      df_subs = subset(df,is_commercial=="Commercial Listing")
+      #print(nrow(df_subs))
+      }
+    else if(input$in_checkboxgroup=="HL"){
+      df_subs = subset(df,is_commercial=="Household Listing")
+      #print(nrow(df_subs))
+      }
+    else{
+      df_subs = df
+      #print(nrow(df_subs))
+      }
+    
+    
+    neighbourhood_costs <- data.frame(aggregate(df_subs$price, by=list(df_subs$neighbourhood_cleansed), FUN=mean))
+    names(neighbourhood_costs) <- c("neighbourhood", "avg_cost")
+    
+    neighbourhood_reviews <- data.frame(aggregate(df_subs$avg_review_value, by=list(df_subs$neighbourhood_cleansed), FUN=mean))
+    names(neighbourhood_reviews) <- c("neighbourhood", "avg_review")
+    
+    neighbourhood_scores <- data.frame(aggregate(df_subs$review_scores_rating, by=list(df_subs$neighbourhood_cleansed), FUN=mean))
+    names(neighbourhood_scores) <- c("neighbourhood", "avg_score")
+    
+    neighbourhood_merged <- merge(neighbourhood_scores, neighbourhood_reviews, by="neighbourhood")
+    neighbourhood_merged <- merge(neighbourhood_merged, neighbourhood_costs, by="neighbourhood")
+    
+    airbnb_neighbourhoods <- geojsonio::geojson_read("../data/neighbourhoods.geojson", what = "sp")
+    pal <- colorNumeric("viridis", NULL)
+    airbnb_neighbourhoods <- sp::merge(airbnb_neighbourhoods, neighbourhood_merged, by="neighbourhood")
+    #print(class(airbnb_neighbourhoods))
+    
+    if(input$radio=="score"){
+      print("SCORE")
+      map <- leafletProxy(mapId = "nymap", session = session,data = airbnb_neighbourhoods) %>%
+        clearShapes() %>%
+        clearControls() %>% 
+        addPolygons(stroke = FALSE, smoothFactor = 0.3, fillOpacity = 1 ,fillColor = ~pal(airbnb_neighbourhoods$avg_score)) %>%
+        leaflet::addLegend(pal = pal, values = ~(airbnb_neighbourhoods$avg_score), opacity = 1.0)
+    }
+    else if(input$radio=="review"){
+      print("REVIEW")
+      map <- leafletProxy(mapId = "nymap", session = session,data = airbnb_neighbourhoods) %>%
+        clearShapes() %>%
+        clearControls() %>% 
+        addPolygons(stroke = FALSE, smoothFactor = 0.3, fillOpacity = 1 ,fillColor = ~pal(airbnb_neighbourhoods$avg_review)) %>%
+        leaflet::addLegend(pal = pal, values = ~(airbnb_neighbourhoods$avg_review), opacity = 1.0)
+    }
+    else if(input$radio=="cost"){
+      print("COST")
+      map <- leafletProxy(mapId = "nymap", session = session,data = airbnb_neighbourhoods) %>%
+        clearShapes() %>%
+        clearControls() %>% 
+        addPolygons(stroke = FALSE, smoothFactor = 0.3, fillOpacity = 1 ,fillColor = ~pal(airbnb_neighbourhoods$avg_cost)) %>%
+        leaflet::addLegend(pal = pal, values = ~(airbnb_neighbourhoods$avg_cost), opacity = 1.0)
+    }
+  })
+  
+})
---
+++ dataviz_app/ui.R
@@ -0,0 +1,68 @@
+library(shiny)
+
+shinyUI(fluidPage(
+  tags$head(includeCSS("styles.css")),
+  # Our fullscreen part of the UI for the map
+  fixedPanel(id = "fullscreen",
+             top = 0, left = 0, 
+             width = "100%", height = "100%", 
+             # Here is the map output in the UI
+             leafletOutput("nymap", 
+                           width = "100%", height = "100%")
+  ), 
+  # And the tabbed part with the inputs and plot
+  absolutePanel(id = "controls", 
+                draggable = TRUE, 
+                top = 30, left = 50, 
+                width = 400, height = "auto",
+                h4("AirBnB Listings"),
+                selectInput("month", label = "Month", 
+                            choices = c("January","February","March","April","May","June","July","August","September","October","November","December"), 
+                            selected = "January"),
+                numericInput("year", 
+                             label = "Year", 
+                             value = 2016, 
+                             min = 2016, 
+                             max = 2017),
+                tabsetPanel(
+                  tabPanel("AirBnB Listings",
+                           numericInput("price", 
+                                        label = "Minimum Price", 
+                                        value = 70, 
+                                        min = 0, 
+                                        max = 10000),
+                           numericInput("quality", 
+                                        label = "Minimum Rating", 
+                                        value = 70, 
+                                        min = 0, 
+                                        max = 100),
+                           numericInput("bedrooms", 
+                                        label = "Minimum Bedrooms", 
+                                        value = 1, 
+                                        min = 0, 
+                                        max = 20),
+                           numericInput("bathrooms", 
+                                        label = "Minimum Bathrooms", 
+                                        value = 1, 
+                                        min = 0, 
+                                        max = 20),
+                          
+                           actionButton('submit','Submit')),
+                  tabPanel("AirBnB Insights",
+                           
+                           checkboxGroupInput("in_checkboxgroup", "Select Listing Type",
+                                              c("Commercial Listing" = "CL",
+                                                "Household Listing" = "HL"),
+                                              selected = NULL),
+            
+                           radioButtons("radio", "Parameters for analysis", selected = character(0),
+                                        choiceNames = list("Cost","Review","Adjusted Listing Score"),
+                                        choiceValues = list("cost", "review", "score")
+                                        ),
+                           
+                           actionButton('submit2','Submit')
+                           
+                           ))
+                
+                
+  )))
